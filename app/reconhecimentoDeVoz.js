c026165b5f79eea37de9aa9ce5850a8a13d33073
Rafael Assakawa <rafael.assakawa@gmail.com>
2023-02-21 14:31:54 -0300

    Commit inicial

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,26 @@
+const elementoChute = document.getElementById('chute');
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+    let chute = e.results[0][0].transcript;
+    chute = chute.replace('.', '');
+    exibeChuteNaTela(chute);
+    verificaSeOChutePossuiUmValorValido(chute);
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start());
